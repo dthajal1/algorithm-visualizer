f66c8e856bfe010b9d2ce154b1c59875b4cc008e
DirajThajali <thajalidiraj@gmail.com>
2023-03-08 11:53:17 -0800

    landing page done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Button } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import { Stack } from "@mui/system";
+
 function App() {
   return (
-    <>
-    </>
+    <Grid container justifyContent='center' alignItems='center' height='90vh'>
+      <Stack spacing={2}>
+        <Button variant="contained" color="secondary" href="https://path-finder101.netlify.app/">
+          Graph Algorithm Visualier
+        </Button>
+        <Button variant="contained" color="secondary" href="https://comparison-sort-visualizer.netlify.app/">
+          Sorting Algorithm Visualier
+        </Button>
+      </Stack>
+    </Grid>
   );
 }
 
